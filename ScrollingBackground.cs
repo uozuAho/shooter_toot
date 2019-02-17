a5331ed8385c657b20cfaebd138733175e889106
Warwick Stone <uozu.aho@gmail.com>
2019-02-17 12:42:33 +1100

    fix bg scrolling off screen

--- ScrollingBackground.cs
+++ ScrollingBackground.cs
@@ -7,24 +7,24 @@ namespace ShooterToot3
     public class ScrollingBackground
     {
         private Texture2D _texture;
-        private Vector2[] _texturePositions;
+        private int[] _texturePositions;
         private int _speed;
         
-        private int _bgHeight;
-        private int _bgWidth;
+        private int _screenHeight;
+        private int _screenWidth;
 
         public void Initialize(ContentManager content, string texturePath, int screenWidth, int screenHeight, int speed)
         {
-            _bgHeight = screenHeight;
-            _bgWidth = screenWidth;
+            _screenHeight = screenHeight;
+            _screenWidth = screenWidth;
 
             _texture = content.Load<Texture2D>(texturePath);
             _speed = speed;
-            _texturePositions = new Vector2[screenWidth / _texture.Width + 2];
+            _texturePositions = new int[2];
 
             for (var i = 0; i < _texturePositions.Length; i++)
             {
-                _texturePositions[i] = new Vector2(i * _texture.Width, 0);
+                _texturePositions[i] = i * screenWidth;
             }
         }
 
@@ -32,20 +32,20 @@ namespace ShooterToot3
         {
             for (var i = 0; i < _texturePositions.Length; i++)
             {
-                _texturePositions[i].X += _speed;
+                _texturePositions[i] += _speed;
 
                 if (_speed <= 0)
                 {
-                    if (_texturePositions[i].X <= -_texture.Width)
+                    if (_texturePositions[i] <= -_screenWidth)
                     {
-                        _texturePositions[i].X = _texture.Width * (_texturePositions.Length - 1);
+                        _texturePositions[i] = _screenWidth * (_texturePositions.Length - 1);
                     }
                 }
                 else
                 {
-                    if (_texturePositions[i].X >= _texture.Width * (_texturePositions.Length - 1))
+                    if (_texturePositions[i] >= _screenWidth * (_texturePositions.Length - 1))
                     {
-                        _texturePositions[i].X = -_texture.Width;
+                        _texturePositions[i] = -_screenWidth;
                     }
                 }
             }
@@ -53,9 +53,9 @@ namespace ShooterToot3
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            for (var i = 0; i < _texturePositions.Length; i++)
+            foreach (var pos in _texturePositions)
             {
-                var rectBg = new Rectangle((int)_texturePositions[i].X, (int)_texturePositions[i].Y, _bgWidth, _bgHeight);
+                var rectBg = new Rectangle(pos, 0, _screenWidth, _screenHeight);
                 spriteBatch.Draw(_texture, rectBg, Color.White);
             }
         }
